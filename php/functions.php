066975389bd43d91bdfc28611846b9733f987861
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-10-26 18:39:15 +0300

    Forms

--- php/functions.php
+++ php/functions.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Functions</title>
 </head>
+
 <body>
     <h1>FUNCTIONS</h1>
 
@@ -12,7 +14,8 @@
     // FUNCTION DEFINITION
     // PARAMETERS
     // Variable Scope
-    function functionName($n) {
+    function functionName($n)
+    {
         // global $a;
         // $a = 2; // Local
         echo "I am inside<br>";
@@ -20,7 +23,7 @@
     }
 
     $a = 123; // Global
-
+    
     // FUNCTION CALL
     functionName($a);
 
@@ -29,13 +32,33 @@
     // Create a function that accepts two numbers, minimum and maximum
     // and prints out the range (difference of the two numbers)
     // Range = MAX - MIN
+    // EXAMPLE:
     // Call:   findRange(5, 256)
     // Output: "Range: 251"
-    function findRange($min, $max) {
-        echo "Range: 251";
+    function findRange($min, $max)
+    {
+        echo "Range: " . ($max - $min);
     }
 
-    findRange(5, 128);
+    findRange(5, 25);
+
+
+    echo '<hr>';
+    // RETURN VALUE
+    function getMin($a, $b)
+    {
+        /* if ($a < $b) {
+            return $a;
+        } else {
+            return $b;
+        } */
+       // Unary Operator ?:
+       return ($a < $b) ? $a  : $b;
+    }
+
+    $min = getMin(155, 111);
+    echo "Minimum: $min";
     ?>
 </body>
+
 </html>
---
+++ tasks/task1_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML 5 Form Input Elements</title>
+</head>
+<body>
+    <h1>HTML 5 Form Input Elements</h1>
+
+    Name: <input type="text" size="50" placeholder="Surname, Given, Middle">
+    <br>
+    Password: <input type="password">
+    <br>
+    Search: <input type="search" placeholder="Search for products">
+    <br>
+    Favorite Number: <input type="number" value="10">
+</body>
+</html>
