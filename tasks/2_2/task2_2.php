3d55558e50984047dd863501301bc71a71ee3752
Ramer Alegre Ramos <ramzxs@gmail.com>
2024-11-16 23:23:21 +0800

    Optimized

--- tasks/2_2/task2_2.php
+++ tasks/2_2/task2_2.php
@@ -1,3 +1,11 @@
+<?php
+// Setup
+date_default_timezone_set('Asia/Kuwait');
+$longFormat = 'dS F Y l h:i:sa';
+$shortFormat = 'Y-m-d H:i:s';
+$daysToAdd = isset($_GET['daysToAdd']) ? $_GET['daysToAdd'] : 1;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,30 +15,15 @@
 </head>
 <body>
     <h1>DATE CALCULATOR APP</h1>
-    
-    <?php
-    date_default_timezone_set('Asia/Kuwait');
-    ?>
 
     <form action="?" method="GET">
         Current Date:
-        <b><?= date('dS F Y l h:i:sa');  ?></b>
+        <b><?= date($longFormat);  ?></b>
         <br>
-
-        <?php /*
-        Manual: https://www.php.net/manual/en/function.date.php
-        date('FORMAT')            // Use current timestamp
-        date('FORMAT', timestamp) // Use manual: strtotime('2024-11-08 22:22')
-        
-        DATE FORMAT:
-        MONTH                DATE            DAY
-        F: November          d: 06           l: Saturday 
-        M: Nov               j: 6            D: Sat
-        m: 11 */ ?>
 
         Add Number of Days:
         <input type="number"
-                value="<?= isset($_GET['daysToAdd']) ? $_GET['daysToAdd'] : 1 ?>"
+                value="<?= $daysToAdd ?>"
                 name="daysToAdd">
         <br>
 
@@ -38,16 +31,11 @@
         <br>
 
         Result:
-        <?php
-        $date = date('Y-m-d H:i:s');
-        $numberOfDays = isset($_GET['daysToAdd']) ? $_GET['daysToAdd'] : 1;
+        <b><?php
+        $date = date($shortFormat);
         
-        /* echo $date;
-        echo '---';
-        echo $numberOfDays;
-        echo '---'; */
-        echo calculate($date, $numberOfDays);
-        ?>
+        echo calculate($date, $daysToAdd);
+        ?></b>
         <br>
     </form>
 </body>
@@ -55,5 +43,23 @@
 
 <?php
 function calculate($date, $numDays) {
-    return $date .' +/- '. $numDays;
+    global $longFormat;
+
+    // '2024-11-16 23:14 + 14 DAY' -> 1732461328 (Unix time)
+    $timestamp = strtotime($date . ' + ' . $numDays . ' DAY');
+  
+    $result = date($longFormat, timestamp: $timestamp);
+    return $result;
 }
+
+
+/*
+Manual: https://www.php.net/manual/en/function.date.php
+date('FORMAT')            // Use current timestamp
+date('FORMAT', timestamp) // Use manual: strtotime('2024-11-08 22:22')
+
+DATE FORMAT:
+MONTH                DATE            DAY
+F: November          d: 06           l: Saturday 
+M: Nov               j: 6            D: Sat
+m: 11 */
